6ff1e18e67c01a069a4cda9e479c685c8db6521e
thrallbata <vicortobdel@gmail.com>
2023-03-05 21:17:01 +0700

    add connect openweather

--- app_weather/views.py
+++ app_weather/views.py
@@ -1,12 +1,33 @@
 from django.shortcuts import render
+from django.conf import settings
+
+from pyowm import OWM
+from pyowm.utils import config
+from pyowm.utils import timestamps
 
 
 def index(request):
     name = 'Hirt'
+    locate = 'Tomsk'
+    weather = get_weather(locate)
 
     data = {
         'name': name,
+        "weather": weather,
+        'locate': locate,
     }
 
     return render(request, 'app_weather/index.html', data)
 
+
+def get_weather(locate):
+    owm = OWM(settings.OPENWEATHER_API_KEY)
+    mgr = owm.weather_manager()
+
+    observation = mgr.weather_at_place(locate)
+    w = observation.weather
+
+    dict_weather = {'status': w.detailed_status, 'temperature': w.temperature('celsius'),
+                    'wind': w.wind(), 'humidity': w.humidity}
+
+    return dict_weather
--- myapp/settings.py
+++ myapp/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'app_weather',
 ]
 
@@ -123,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+OPENWEATHER_API_KEY = '01c839bc5a4b37959d3fea4783cbf696'
